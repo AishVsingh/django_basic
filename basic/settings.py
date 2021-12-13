2febc709a32a1f16f0eebe7e276faca76c2569f7
Aish Singh <aishv.585@gmail.com>
2021-12-13 17:59:17 +0000

    Adding static files

--- basic/settings.py
+++ basic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,7 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
